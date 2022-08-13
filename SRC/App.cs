bedccad706d55f362bd0000de4b9e4dc79aa69b9
Yasmin Meirelles <yasminmeirellessanchez@gmail.com>
2022-08-13 20:07:39 -0300

    react

---
+++ SRC/App.cs
@@ -0,0 +1,23 @@
+.App {
+  text-align: center;
+}
+
+.App-foto{
+  height: 40vmin;
+}
+
+.App-header {
+  background-color: rgb(17, 16, 16);
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+  font-weight: bold;
+}
+
+.App-link {
+  color: #61dafb;
+}
---
+++ SRC/App.js
@@ -0,0 +1,19 @@
+import foto from './foto.jpg'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={foto} className="App-foto" alt="Foto de perfil" />
+        <p>
+          Eduardo Guimarães
+        </p>
+        <a className="App-link" href="https://github.com/Eduardo-GAlves" target="_blank" rel="noopener noreferrer">
+          Visite meu Github
+        </a>
+      </header>
+    </div>
+  );
+}
+export default App;
